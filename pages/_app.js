395ae2f43e4fd9744af3f9b8180c1b6cca61d244
rachel490 <ccori12@naver.com>
2021-12-06 03:49:04 +0900

    [feat] add global state for playlist & playlistId w/ Recoil

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import { SessionProvider } from 'next-auth/react'
-import 'tailwindcss/tailwind.css'
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+import "tailwindcss/tailwind.css";
 
-function MyApp({ Component, pageProps: { session, ...pageProps} }) {
-  return (
-    <SessionProvider session={session }>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	return (
+		<SessionProvider session={session}>
+			<RecoilRoot>
+				<Component {...pageProps} />
+			</RecoilRoot>
+		</SessionProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
